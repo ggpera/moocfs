bede72e06626b280fbb745494b3b5d777e45a612
Petri <peewter@gmail.com>
2022-06-14 20:26:31 +0300

    1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,22 +1,25 @@
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1}
-                     part2={part2}
-                     part3={part3} 
-                     exercises1={exercises1} 
-                     exercises2={exercises2} 
-                     exercises3={exercises3} />
-            <Total total={exercises1 + exercises2 + exercises3} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     )
 }
@@ -33,7 +36,7 @@ const Part = (props) => {
     return (
         <div>
             <p>
-                {props.parts} {props.exercises}
+                {props.parts.name} {props.parts.exercises}
             </p>
         </div>
     )
@@ -42,9 +45,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <div>
-            <Part parts={props.part1} exercises={props.exercises1} />
-            <Part parts={props.part2} exercises={props.exercises2} />
-            <Part parts={props.part3} exercises={props.exercises3} />
+            <Part parts={props.parts[0]} />
+            <Part parts={props.parts[1]} />
+            <Part parts={props.parts[2]} />
         </div>
     )
 }
@@ -53,7 +56,9 @@ const Total = (props) => {
     return (
         <div>
             <p>
-                Number of exercises {props.total}
+                Number of exercises {props.parts[0].exercises + 
+                                     props.parts[1].exercises + 
+                                     props.parts[2].exercises}
             </p>
         </div>
     )
