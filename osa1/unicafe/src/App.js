8cfdbeb309697e56ed33d1b8f791e3671ebd148b
Petri <peewter@gmail.com>
2022-06-20 17:30:37 +0300

    add unicafe 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -6,22 +6,55 @@ const Button = ({ handleClick, text }) => (
     </button>
 )
 
+const Positive = (props) => {
+    if (props.good === 0) {
+        return (
+            <div>
+                positive 0 %
+            </div>
+        )
+    }
+    return (
+        <div>
+            positive {(props.good / props.total) * 100} %
+        </div>
+    )
+}
+
+const Average = (props) => {
+    if (props.total === 0) 
+        return (
+            <div>
+                average 0
+            </div>
+        )
+    return (
+        <div>
+            average {((props.good * 1) + (props.neutral * 0) + (props.bad * -1)) / props.total}
+        </div>
+    )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
+    // tallenna napit omaan tilaansa
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [total, setTotal] = useState(0)
 
     const handleGoodClick = () => {
         setGood(good + 1)
+        setTotal(total + 1)
     }
 
     const handleNeutralClick = () => {
         setNeutral(neutral + 1) 
+        setTotal(total + 1)
     }
 
     const handleBadClick = () => {
-        setBad(bad + 1) 
+        setBad(bad + 1)
+        setTotal(total + 1)
     }
 
      return (
@@ -34,9 +67,12 @@ const App = () => {
             </div>
             <div>
                 <h1>statistics</h1>
-                <p>good {good}</p>
-                <p>neutral {neutral}</p>
-                <p>bad {bad}</p>
+                <div>good {good}</div>
+                <div>neutral {neutral}</div>
+                <div>bad {bad}</div>
+                <div>all {total}</div>
+                <Average total={total} good={good} neutral={neutral} bad={bad} />
+                <Positive good={good} total={total} />
             </div>
         </div>
     )
