4da80f40c82c9fa24b3604f21b571d625e54743b
Petri <peewter@gmail.com>
2022-06-30 18:39:23 +0300

    add puhelinluettelo 2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,30 +2,41 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { 
+            name: 'Arto Hellas',
+            number: '040-12341456',
+        }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
     const addName = (event) => {
         event.preventDefault()
 
         if (persons.some(person => person.name === newName)) {
             alert(`${newName} is already added to the phonebook`)
+            setNewNumber('')
             setNewName('')
             return
         }
         else {
             const nameObject = {
                 name: newName,
+                number: newNumber
             }
             setPersons(persons.concat(nameObject))
             setNewName('')
+            setNewNumber('')
         }
     }
 
     const handleNameChange = (event) => {
         console.log(event.target.value)
         setNewName(event.target.value)
+    }
+    const handleNumberChange = (event) => {
+        console.log(event.target.value)
+        setNewNumber(event.target.value)
     }
 
     return (
@@ -40,16 +51,24 @@ const App = () => {
                         />
                 </div>
                 <div>
+                    number:
+                        <input
+                            value={newNumber}
+                            onChange={handleNumberChange}
+                        />
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
+                
             </form>
             <h2>Numbers</h2>
             <div>
-                {persons.map(person => 
-                    <p key={person.name}>
-                        {person.name}
-                    </p>
-                )}
+            {persons.map(person =>
+                <p key={person.name}>
+                    {person.name} {' '} {person.number}
+                </p>
+            )}
             </div>
         </div>
     )
