772f99eb8da73bd63764e786946d4dce8a6ee5e5
Petri <peewter@gmail.com>
2022-07-06 15:04:55 +0300

    add puhelinluettelo 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { 
-            name: 'Arto Hellas',
-            number: '040-12341456',
-        }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setNewFilter] = useState('')
 
     const addName = (event) => {
         event.preventDefault()
@@ -31,17 +32,29 @@ const App = () => {
     }
 
     const handleNameChange = (event) => {
-        console.log(event.target.value)
         setNewName(event.target.value)
     }
     const handleNumberChange = (event) => {
-        console.log(event.target.value)
         setNewNumber(event.target.value)
     }
+    const handleFilterChange = (event) => {
+        setNewFilter(event.target.value)
+    }
+
+    const filtered = persons.filter(p =>
+                     p.name.toLowerCase().includes(newFilter.toLowerCase()))
 
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter: 
+                    <input
+                        value={newFilter}
+                        onChange={handleFilterChange}
+                    />
+            </div>
+            <h2>Add a new contact</h2>
             <form onSubmit={addName}>
                 <div>
                     name: 
@@ -64,7 +77,7 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <div>
-            {persons.map(person =>
+            {filtered.map(person =>
                 <p key={person.name}>
                     {person.name} {' '} {person.number}
                 </p>
